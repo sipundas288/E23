c298ea38b085a7065f0c03a1b18c507e5788cda4
sipun <sipun@SipunDas>
2025-08-22 08:32:44 +0530

    Commit code to git

---
+++ SampleMaven/src/test/java/demo/Sample.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+	}
+
+}
