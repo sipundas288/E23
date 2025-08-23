05bedc95007b8ab759c3ee2d7b8eac9b1fc45b70
sipun <sipun@SipunDas>
2025-08-23 08:16:52 +0530

    Merge branch 'master' of https://github.com/sipundas288/E23.git

---
+++ SampleMaven/src/test/java/demo/NewOne.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class NewOne {
+
+	public static void main(String[] args) {
+		System.out.println("New One");
+
+	}
+
+}
--- SampleMaven/src/test/java/demo/SampleDemoOne.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class SampleDemoOne {
-
-	public static void main(String[] args) {
-		System.out.println("Good Morning Everyone");
-	}
-
-}
